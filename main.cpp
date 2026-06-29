5e52289e29046e3576c8a44c799705be9379602c
Ptalemon <oleg.fed2006@gmail.com>
2026-06-29 13:55:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <menu.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    LibraryDB* library = new LibraryDB();
+
+    string filename = "library.dat";
+
+    Menu* menu = new Menu(*library, filename);
+
+    menu->runMenu();
+
+    delete menu;
+    delete library;
+
     return 0;
 }
