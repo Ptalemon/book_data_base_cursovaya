fef905692eaa67ae1cb8bf7e8059937090a82692
Ptalemon <oleg.fed2006@gmail.com>
2026-06-29 13:56:26 +0300

    Update transaction.h

--- transaction.h
+++ transaction.h
@@ -1,25 +1,43 @@
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
-#include <Student.h>
 #include <string>
 #include <iostream>
+#include <fstream>
 
-struct transaction{
-    student student_data;
-    string issue_date;
-    string return_date; //If NULL - the book is taken
-    string librarian;
-};
+using namespace std;
 
-class Transaction: public Student
+class Transaction
 {
-    transaction transaction_data;
+    string TransactionID;
+    string stud_ID;
+    string book_inv_ID;
+    string issueDate;
+    string returnDate;
+    string status;
+    string librarian;
 public:
-    Transaction(student, string, string, string);
-    ~Transaction();
-    Transaction(const Transaction&, const student&);
+    Transaction();
+    Transaction(string, string, string, string, string, string, string);
 
-    void close_transaction();
+    void close_transaction(string);
+    void display();
+
+    string getID(){return TransactionID;}
+    string getStudID(){return stud_ID;}
+    string getInvNum(){return book_inv_ID;}
+    string getStatus(){return status;}
+
+    void setStatus(string newStatus){status = newStatus;}
+    void setRtrnDate(string newRtrnDate){returnDate = newRtrnDate;}
+    void setLibrarian(string newLinrarian){librarian = newLinrarian;}
+    void setID(string newID){TransactionID = newID;}
+    void setStudID(string newStudID){stud_ID = newStudID;}
+    void setBookID(string newBookID){book_inv_ID = newBookID;}
+    void setIssDate(string newIssDate){issueDate = newIssDate;}
+
+    void save_to_file(ofstream&) const;
+    void load_from_file(ifstream&);
+
 };
 
 #endif // TRANSACTION_H
