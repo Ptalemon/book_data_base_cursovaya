37cb8c2291cc92b839ed07f765f034866b4a810c
Ptalemon <oleg.fed2006@gmail.com>
2026-06-29 13:55:32 +0300

    Update book.h

--- book.h
+++ book.h
@@ -2,29 +2,40 @@
 #define BOOK_H
 #include <string>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-struct book{
-    int book_id;
-    string author;
-    string title;
-    int year_published;
-    int total_copies;
-    int available_copies;
-};
 
 class Book
 {
-    book data_book;
+    string book_id;
+    string author;
+    string title;
+    int total_copies;
+    int available_copies;
 public:
-    Book(int, string, string, int, int, int);
-    Book(book);
-    ~Book();
-    Book(const Book&);
+    Book();
+    Book(string, string, string, int, int);
 
-    bool check_availability();
-    void update_copies(int, int);
+    void display();
+
+
+    bool isAvailable();
+    string getID(){return book_id;}
+    void setID(string newID){book_id = newID;}
+    void setAuthor(string newAuthor){author = newAuthor;}
+    void setTitle(string newTitle){title = newTitle;}
+    void setTotal(int newTotal){total_copies = newTotal;}
+    void setAvail(int newAvail){available_copies = newAvail;}
+
+
+    void issue_book();
+    void return_book();
+
+    void save_to_file(ofstream&) const;
+    void load_from_file(ifstream&);
+
 };
 
 #endif // BOOK_H
