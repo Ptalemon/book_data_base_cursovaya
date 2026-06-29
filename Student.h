0d7dec1e755a421d500ec2eac8bb69a918f7bd7f
Ptalemon <oleg.fed2006@gmail.com>
2026-06-29 13:54:57 +0300

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,34 +1,35 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-#include <book.h>
 #include <string>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-struct student{
-    string first_name;
-    string last_name;
-    string middle_name;
-    string ent_date;
+
+class Student
+{
+    string name;
+    int ent_date;
     string group;
     string record_book_id;
     string institute;
-    book taken_book;
-};
-
-
-class Student:public Book
-{
-    student data_student;
 public:
-    Student(int, string, string, int, int, int, string, string,
-            string, string, string, string, string);
-    ~Student();
-    Student(student);
-    Student(const Student&, const book&);
+    Student();
+    Student(string, int, string, string, string);
 
-    void get_borrowed_books();
+    void display();
+
+    string getID(){return record_book_id;}
+    void setName(string newName){name = newName;}
+    void setGroup(string newGroup){group = newGroup;}
+    void setInstitute(string newInstitute){institute = newInstitute;}
+    void setEntDate(int newEntDate){ent_date = newEntDate;}
+    void setRecBookID(string newRecBookID){record_book_id = newRecBookID;}
+
+    void save_to_file(ofstream&) const;
+    void load_from_file(ifstream&);
+
 };
 
 #endif // BASE_DB_H
